f8f821989da39e3edc211eafed23711553f5febc
Neosh <neosh11@gmail.com>
2024-11-30 11:41:07 +1100

    Faster load times

--- second-star/astro.config.mjs
+++ second-star/astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs(), purgecss()]
 });
