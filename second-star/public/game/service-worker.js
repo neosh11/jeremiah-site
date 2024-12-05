09ed19e8b3c7378313e4ba6dd088194fce77ed2f
Neosh <neosh11@gmail.com>
2024-12-05 14:12:57 +1100

    Add cards

--- second-star/public/game/service-worker.js
+++ second-star/public/game/service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'jeremiahsparty';
+var cacheName = 'jeremiah-s-party';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
